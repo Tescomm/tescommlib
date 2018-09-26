f57b4d1ed0d4ad996c6b4529f562611d8a2c3027
sinyscar <sinyscar@163.com>
2018-09-26 15:42:14 +0800

    frist commit

---
+++ utilcode/src/main/java/com/tescomm/utilcode/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tescomm.utilcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static com.tescomm.utilcode.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
